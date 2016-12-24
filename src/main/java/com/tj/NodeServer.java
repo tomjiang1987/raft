540dc39cd5f5def14696fddd7ea63b3bd0162b4e
tom.jiang1987@gmail.com <tom.jiang1987@gmail.com>
2016-12-24 21:53:31 +0800

    add gitignore

--- src/main/java/com/tj/NodeServer.java
+++ src/main/java/com/tj/NodeServer.java
@@ -7,6 +7,5 @@ public class NodeServer {
   public static void main(String[] args){
 	  Logger logger = LoggerFactory.getLogger(NodeServer.class);
 	  logger.info("node[{}] start ", 1);
-	  
   }
 }
