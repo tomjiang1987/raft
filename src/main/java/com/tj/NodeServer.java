8186a998d93d2c221b1563ae8003825e41639d16
tom.jiang1987@gmail.com <tom.jiang1987@gmail.com>
2016-12-24 21:47:07 +0800

    add logger

---
+++ src/main/java/com/tj/NodeServer.java
@@ -0,0 +1,12 @@
+package com.tj;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NodeServer {
+  public static void main(String[] args){
+	  Logger logger = LoggerFactory.getLogger(NodeServer.class);
+	  logger.info("node[{}] start ", 1);
+	  
+  }
+}
